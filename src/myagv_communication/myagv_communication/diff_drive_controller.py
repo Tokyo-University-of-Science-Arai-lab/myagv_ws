3d59d174cec2c3deed86514b507836267d5aaed3
7521109 <7521109@example.com>
2024-07-19 13:25:19 +0900

    second commit

--- src/myagv_communication/myagv_communication/diff_drive_controller.py
+++ src/myagv_communication/myagv_communication/diff_drive_controller.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -11,30 +11,37 @@ class DiffDriveController(Node):
         self.subscription = self.create_subscription(Twist, 'cmd_vel', self.listener_callback, 10)
         self.motor_cont = Float32MultiArray()
     
-    def listener_callback(self, Twist):
+    def listener_callback(self, twist):
         d = 0.35
-        vel_r = (Twist.linear.x + d*-Twist.angular.z)*240    #385(0.95)
-        vel_l = (Twist.linear.x - d*-Twist.angular.z)*234    #232 
-
-        '''    
+        vel_r = (twist.linear.x + d * twist.angular.z) * 240    # 385(0.95)
+        vel_l = (twist.linear.x - d * twist.angular.z) * 234    # 232 
+        
+        if 0 < vel_r < 30:
+            vel_r = 30
+        elif -30 < vel_r < 0:
+            vel_r = -30
+        
+        if 0 < vel_l < 30:
+            vel_l = 30
+        elif -30 < vel_l < 0:
+            vel_l = -30
+        
+        '''
         if vel_r < 0:
-            vel_r =0
+            vel_r = 0
         elif vel_r > 120:
             vel_r = 120
         
         if vel_l < 0:
-            vel_l =0
+            vel_l = 0
         elif vel_l > 120:
             vel_l = 120
         '''
         
-        self.get_logger().info("Velocity: right=%f" % (vel_r))
-        self.get_logger().info("Velocity: left=%f" % (vel_l))   
+        self.get_logger().info("Velocity: right=%f" % vel_r)
+        self.get_logger().info("Velocity: left=%f" % vel_l)   
 
-        array = []
-        array.append(vel_l)
-        array.append(vel_r)
-
+        array = [vel_l, vel_r]
         self.motor_cont = Float32MultiArray(data=array)
         self.publisher.publish(self.motor_cont)
 
@@ -46,3 +53,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
