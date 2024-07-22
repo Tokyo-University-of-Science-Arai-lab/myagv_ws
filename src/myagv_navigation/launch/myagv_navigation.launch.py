bd899b7d19c186fd93686ea08d491750cc76b61b
Sato Taisuke <7521053@ed.tus.ac.jp>
2024-07-22 18:03:33 +0900

    navigation_launch make

--- src/myagv_navigation/launch/myagv_navigation.launch.py
+++ src/myagv_navigation/launch/myagv_navigation.launch.py
@@ -1,165 +1,188 @@
-from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 import os
 
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, GroupAction,
+                            IncludeLaunchDescription, SetEnvironmentVariable)
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml, ReplaceString
+
+
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='False')
-    map_yaml_file = LaunchConfiguration('map', default='/home/agv2/myagv_ws_proto2/src/myagv_navigation/map/map_1718364393.yaml')
-    params_file = LaunchConfiguration('params_file', default='/home/agv2/myagv_ws_proto2/src/my_param/agv1_param.yaml')
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    launch_dir = os.path.join(bringup_dir, 'launch')
 
-    # Nav2 bringup launch
-    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    # 自分たちのマップとパラメータファイルのパスを設定
+    custom_map_file = os.path.join(get_package_share_directory('myagv_navigation'), 'map', 'map_1721186946.yaml')
+    custom_params_file = os.path.join(get_package_share_directory('my_param'), 'agv1_param.yaml')
 
-    nav2_bringup_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')),
-        launch_arguments={'use_sim_time': use_sim_time,
-                          'map': map_yaml_file,
-                          'params_file': params_file}.items(),
-    )
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    slam = LaunchConfiguration('slam')
+    map_yaml_file = LaunchConfiguration('map', default=custom_map_file)  # ここでデフォルトのマップファイルを指定
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    params_file = LaunchConfiguration('params_file', default=custom_params_file)  # ここでデフォルトのパラメータファイルを指定
+    autostart = LaunchConfiguration('autostart')
+    use_composition = LaunchConfiguration('use_composition')
+    use_respawn = LaunchConfiguration('use_respawn')
+    log_level = LaunchConfiguration('log_level')
 
-    rviz_config_file = os.path.join(nav2_bringup_dir, 'rviz', 'nav2_default_view.rviz')
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
 
-    # RViz node
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config_file],
-        parameters=[{'use_sim_time': use_sim_time}]
-    )
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {
+        'use_sim_time': use_sim_time,
+        'yaml_filename': map_yaml_file}
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='False',
-            description='Use simulation (Gazebo) clock if true'
-        ),
-        DeclareLaunchArgument(
-            'map',
-            default_value='/home/agv2/myagv_ws_proto2/src/myagv_navigation/map/map_1718364393.yaml',
-            description='Full path to map yaml file to load'
-        ),
-        DeclareLaunchArgument(
-            'params_file',
-            default_value='/home/agv2/myagv_ws_proto2/src/my_param/agv1_param.yaml',
-            description='Full path to the ROS2 parameters file to use for all launched nodes'
-        ),
-        nav2_bringup_launch,
-        rviz_node
+    # Only it applys when `use_namespace` is True.
+    # '<robot_namespace>' keyword shall be replaced by 'namespace' launch argument
+    # in config file 'nav2_multirobot_params.yaml' as a default & example.
+    # User defined config file should contain '<robot_namespace>' keyword for the replacements.
+    params_file = ReplaceString(
+        source_file=params_file,
+        replacements={'<robot_namespace>': ('/', namespace)},
+        condition=IfCondition(use_namespace))
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True),
+        allow_substs=True)
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        'RCUTILS_LOGGING_BUFFERED_STREAM', '1')
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the navigation stack')
+
+    declare_slam_cmd = DeclareLaunchArgument(
+        'slam',
+        default_value='False',
+        description='Whether run a SLAM')
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        default_value=custom_map_file,
+        description='Full path to map yaml file to load')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        'params_file',
+        default_value=custom_params_file,
+        description='Full path to the ROS2 parameters file to use for all launched nodes')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        'autostart', default_value='true',
+        description='Automatically startup the nav2 stack')
+
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        'use_composition', default_value='True',
+        description='Whether to use composed bringup')
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        'use_respawn', default_value='False',
+        description='Whether to respawn if a node crashes. Applied when composition is disabled.')
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        'log_level', default_value='info',
+        description='log level')
+
+    # Specify the actions
+    bringup_cmd_group = GroupAction([
+        PushRosNamespace(
+            condition=IfCondition(use_namespace),
+            namespace=namespace),
+
+        Node(
+            condition=IfCondition(use_composition),
+            name='nav2_container',
+            package='rclcpp_components',
+            executable='component_container_isolated',
+            parameters=[configured_params, {'autostart': autostart}],
+            arguments=['--ros-args', '--log-level', log_level],
+            remappings=remappings,
+            output='screen'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'slam_launch.py')),
+            condition=IfCondition(slam),
+            launch_arguments={'namespace': namespace,
+                              'use_sim_time': use_sim_time,
+                              'autostart': autostart,
+                              'use_respawn': use_respawn,
+                              'params_file': params_file}.items()),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir,
+                                                       'localization_launch.py')),
+            condition=IfCondition(PythonExpression(['not ', slam])),
+            launch_arguments={'namespace': namespace,
+                              'map': map_yaml_file,
+                              'use_sim_time': use_sim_time,
+                              'autostart': autostart,
+                              'params_file': params_file,
+                              'use_composition': use_composition,
+                              'use_respawn': use_respawn,
+                              'container_name': 'nav2_container'}.items()),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'navigation_launch.py')),
+            launch_arguments={'namespace': namespace,
+                              'use_sim_time': use_sim_time,
+                              'autostart': autostart,
+                              'params_file': params_file,
+                              'use_composition': use_composition,
+                              'use_respawn': use_respawn,
+                              'container_name': 'nav2_container'}.items()),
     ])
 
-# /home/agv1/agv1_ws/map_1718364393.yaml'
-# from launch import LaunchDescription
-# from ament_index_python.packages import get_package_share_directory
-# from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-# from launch.launch_description_sources import PythonLaunchDescriptionSource
-# from launch.substitutions import LaunchConfiguration
-# from launch_ros.actions import Node
-# import os
+    # Create the launch description and populate
+    ld = LaunchDescription()
 
-# def generate_launch_description():
-#     use_sim_time = LaunchConfiguration('use_sim_time', default='False')
-#     map_yaml_file = LaunchConfiguration('map', default='/home/agv1/agv1_ws/map_1718364393.yaml')
-#     params_file = LaunchConfiguration('params_file', default='/home/agv1/myagv_ws/src/myagv_navigation/config/my_param/agv1_param.yaml')
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
 
-#     # Nav2 bringup launch
-#     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_slam_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_composition_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
 
-#     nav2_bringup_launch = IncludeLaunchDescription(
-#         PythonLaunchDescriptionSource(os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')),
-#         launch_arguments={'use_sim_time': use_sim_time,
-#                           'map': map_yaml_file,
-#                           'params_file': params_file}.items(),
-#     )
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(bringup_cmd_group)
 
-#     rviz_config_file = os.path.join(nav2_bringup_dir, 'rviz', 'nav2_default_view.rviz')
-
-#     # RViz node
-#     rviz_node = Node(
-#         package='rviz2',
-#         executable='rviz2',
-#         name='rviz2',
-#         output='screen',
-#         arguments=['-d', rviz_config_file],
-#         parameters=[{'use_sim_time': use_sim_time}]
-#     )
-
-#     return LaunchDescription([
-#         DeclareLaunchArgument(
-#             'use_sim_time',
-#             default_value='False',
-#             description='Use simulation (Gazebo) clock if true'
-#         ),
-#         DeclareLaunchArgument(
-#             'map',
-#             default_value='/home/agv1/agv1_ws/map_1718364393.yaml',
-#             description='Full path to map yaml file to load'
-#         ),
-#         DeclareLaunchArgument(
-#             'params_file',
-#             default_value='/home/agv1/myagv_ws/src/myagv_navigation/config/agv1_param.yaml',
-#             description='Full path to the ROS2 parameters file to use for all launched nodes'
-#         ),
-#         nav2_bringup_launch,
-#         rviz_node
-#     ])
-
-# from launch import LaunchDescription
-# from ament_index_python.packages import get_package_share_directory
-# from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-# from launch.launch_description_sources import PythonLaunchDescriptionSource
-# from launch.substitutions import LaunchConfiguration
-# from launch_ros.actions import Node
-# import os
-
-# def generate_launch_description():
-#     use_sim_time = LaunchConfiguration('use_sim_time', default='False')
-#     map_yaml_file = LaunchConfiguration('map', default='/home/agv1/agv1_ws/map_1718364393.yaml')
-#     params_file = LaunchConfiguration('params_file', default='/home/agv1/myagv_ws/src/myagv_navigation/config/agv1_param.yaml')
-
-#     # Nav2 bringup launch
-#     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
-
-#     nav2_bringup_launch = IncludeLaunchDescription(
-#         PythonLaunchDescriptionSource(os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')),
-#         launch_arguments={'use_sim_time': use_sim_time,
-#                           'map': map_yaml_file,
-#                           'params_file': params_file}.items(),
-#     )
-
-#     rviz_config_file = os.path.join(nav2_bringup_dir, 'rviz', 'nav2_default_view.rviz')
-
-#     # RViz node
-#     rviz_node = Node(
-#         package='rviz2',
-#         executable='rviz2',
-#         name='rviz2',
-#         output='screen',
-#         arguments=['-d', rviz_config_file],
-#         parameters=[{'use_sim_time': use_sim_time}]
-#     )
-
-#     return LaunchDescription([
-#         DeclareLaunchArgument(
-#             'use_sim_time',
-#             default_value='False',
-#             description='Use simulation (Gazebo) clock if true'
-#         ),
-#         DeclareLaunchArgument(
-#             'map',
-#             default_value='/home/agv1/agv1_ws/map_1718364393.yaml',
-#             description='Full path to map yaml file to load'
-#         ),
-#         DeclareLaunchArgument(
-#             'params_file',
-#             default_value='/home/agv1/myagv_ws/src/myagv_navigation/config/agv1_param.yaml',
-#             description='Full path to the ROS2 parameters file to use for all launched nodes'
-#         ),
-#         nav2_bringup_launch,
-#         rviz_node
-#     ])
+    return ld
